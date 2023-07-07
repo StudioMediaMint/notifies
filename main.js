5b0eddfcad5eee48309a770f03d84afa0ec6bb6e
Leonid Bychkov <bychkov.l47@mail.ru>
2023-07-08 02:21:23 +0300

    поправил визуал согласно Figma

--- main.js
+++ main.js
@@ -1,34 +1,132 @@
 document.addEventListener("DOMContentLoaded", () => {
     const t = 500000;
 
-    const notify = new Notify("", {
+    // слева сверху
+    const notify_left_1 = new Notify("", {
         type: "info",
+        title: "Информация",
         closeTimeout: t,
         position: "top left",
-        text: "слева сверху"
+        text: "Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек.",
+        filled: true,
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
     })
-    const noty1 = new Notify("", {
+    const notify_left_2 = new Notify("", {
+        type: "warning",
+        title: "Ахтунг! Алярм! Внимание!",
+        closeTimeout: t,
+        position: "top left",
+        text: "Тут будет текст предупреждения",
+        button: {
+            text: "Открыть ещё раз",
+            callback: () => {},
+        },
+    })
+    const notify_left_3 = new Notify("", {
+        type: "info",
+        title: "Информация",
+        closeTimeout: t,
+        position: "top left",
+        text: "Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек.",
+    })
+
+    // слева снизу
+    const noty_bottom_left_1 = new Notify("", {
         type: "error",
+        title: 'Ошибка!',
         closeTimeout: t,
         position: "bottom left",
         text: "снизу слева",
+        filled: true,
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
     })
-    const noty2 = new Notify("", {
+    const noty_bottom_left_2 = new Notify("", {
         type: "success",
+        title: 'Успешный успех!!!',
+        closeTimeout: t,
+        position: "bottom left",
+        text: "снизу слева",
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
+    })
+
+    // справа снизу
+    const noty_right_bottom_1 = new Notify("", {
+        type: "success",
+        title: "Успех!",
         position: "bottom right",
+        filled: true,
         closeTimeout: t,
         text: "справа снизу",
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
     })
-    const noty3 = new Notify("", {
+    const noty_right_bottom_2 = new Notify("", {
+        type: "error",
+        title: "Ошибка",
+        position: "bottom right",
+        closeTimeout: t,
+        text: "Попробуйте отправить форму ещё раз",
+    })
+
+    // право верх
+    const notify_right_2 = new Notify("", {
         type: "success",
+        title: "Информация",
+        closeTimeout: t,
+        filled: true,
+        position: "top right",
+        text: "Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек. Текст в несколько строчек.",
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
+    })
+
+    const noty3 = new Notify("", {
+        type: "warning",
+        title: "Предупреждение",
+        filled: true,
         closeTimeout: t,
         text: "Справа сверху",
+        button: {
+            text: "Текст кнопки",
+            callback: () => {},
+        },
     })
 
     setTimeout(() => {
-        notify.show();
-        noty1.show();
-        noty2.show();
+        notify_left_1.show();
+        notify_left_2.show();
+        notify_left_3.show();
+        notify_left_1.show();
+        notify_left_2.show();
+        notify_left_3.show();
+
+        noty_bottom_left_1.show();
+        noty_bottom_left_2.show();
+        noty_bottom_left_1.show();
+        noty_bottom_left_2.show();
+        noty_bottom_left_1.show();
+        noty_bottom_left_2.show();
+        noty_bottom_left_1.show();
+        noty_bottom_left_2.show();
+
+
+        noty_right_bottom_1.show();
+        noty_right_bottom_2.show();
+
         noty3.show();
+        notify_right_2.show();
     }, 1000);
 })
--- src/notifies.js
+++ src/notifies.js
@@ -1,63 +1,50 @@
-// function getCounter() {
-//     let counter = 1;
-//     return function inner () {
-//         return counter++;
-//     }
-// }
-//
-// const counter = getCounter();
-
 class Notify {
-    // определяет, куда выводить HTML, если
-    _whereToRender;
-
-    // тип сообщения: Ошибка, Инфо, Успех
-    _type;
-
-    // Выводить ошибку как всплывающую в fixed-блок или выводить ошибку в заранее подготовленное место
-    _position;
-
-    // если выводить как всплывающую, то где? слева/справа внизу/наверху
-    _cssPosition;
-
-    // куда выводить ошибку. Если _position==="block", то берется _whereToRender, иначе ".mm-notifies-wrapper.fixed";
-    _selector;
-
-    // текст сообщения
-    _notifyText;
-
-    // время пропадания сообщения при _position==="fixed";
-    _closeTime;
-
-    // Сюда помещается результат setTimeout(() => {}, _closeTime);
-    _closeTimeoutFunction;
-
-    // DOM-элемент уведомления
-    _notyElem;
+    #whereToRender          // определяет, куда выводить HTML, если
+    #type                   // тип сообщения: Ошибка, Инфо, Успех, Предупреждение
+    #notyTitle              // заголовок сообщения. Зависит от type
+    #position               // Выводить ошибку как всплывающую в fixed-блок или выводить ошибку в заранее подготовленное место
+    #cssPosition            // если выводить как всплывающую, то где? слева/справа внизу/наверху
+    #selector               // куда выводить ошибку. Если #position==="block", то берется #whereToRender, иначе ".mm-notifies-wrapper.fixed";
+    #notifyText             // текст сообщения
+    //#closeTime              // время пропадания сообщения при position==="fixed";
+    #closeTimeoutFunction   // Сюда помещается результат setTimeout(() => {}, _closeTime);
+    //#notyElem               // DOM-элемент уведомления
+    #hasButton              // есть ли кнопка в уведомлении
+    #buttonConfig           // конфигурация кнопки
+    #filledBackground       // будет ли уведомление залито своим цветом
 
     constructor(whereToRender, options = {
         type: "info",
+        title: "Информация",
         position: "top right",
         cssPosition: "fixed",
         text: "Сообщение",
         closeTimeout: 3000,
     }) {
         // "info" || "success" || "error"
-        this._type = options.type ? options.type : "info";
+        this.#type = options.type ? options.type : "info";
+        this.#notyTitle = options.title ? options.title : "Информация";
 
-        this._whereToRender = whereToRender ? whereToRender : null,
+        this.#filledBackground = options.filled ? options.filled : false;
+
+        this.#whereToRender = whereToRender ? whereToRender : null;
 
         // "top right" || "top left" || "bottom left" || "bottom right"
-        this._position = options.position ? options.position : "top right";
+        this.#position = options.position ? options.position : "top right";
 
         // "fixed" || "block"
-        this._cssPosition = options.cssPosition ? options.cssPosition : "fixed";
+        this.#cssPosition = options.cssPosition ? options.cssPosition : "fixed";
 
-        this._selector = options.cssPosition === "block" ? whereToRender : ".mm-notifies-wrapper.fixed";
-        this._notifyText = options.text ? options.text : "Сообщение";
+        this.#selector = options.cssPosition === "block" ? whereToRender : ".mm-notifies-wrapper.fixed";
+        this.#notifyText = options.text ? options.text : "Сообщение";
         // this.counterId = counter();
-        this._closeTime = options.closeTimeout ? options.closeTimeout : 3000,
-        this._closeTimeoutFunction = null;
+        this.closeTime = options.closeTimeout ? options.closeTimeout : 3000;
+        this.#closeTimeoutFunction = null;
+
+        this.#hasButton = options.hasOwnProperty("button");
+        this.#buttonConfig = options.button ? options.button : null;
+
+        this.close.bind(this);
     }
 
     getTemplate() {
@@ -66,36 +53,60 @@ class Notify {
         let templateElem = null;
 
         // рисуем просто текст ошибки
-        if (this._whereToRender) {
+        if (this.#whereToRender) {
             templateElem = document.createElement("p");
-            templateElem.classList.add("mm-notify__text-noty", `${this._type}`)
-            templateElem.textContent = `${this._notifyText}`;
+            templateElem.classList.add("mm-notify__text-noty", `${this.#type}`)
+            templateElem.textContent = `${this.#notifyText}`;
         }
         // рисуем блок с иконкой
         else {
             templateElem = document.createElement("div");
-            templateElem.classList.add("mm-notify", `${this._type}`);
-            // templateElem.dataset.notifyCounter = `${this.counterId}`;
+            templateElem.classList.add("mm-notify", `${this.#type}`);
+
+            this.#filledBackground ? templateElem.classList.add("filled") : null;
 
             templateElem.innerHTML = `
-                <img src="./icons/${this._type}.svg"
+                <img src="./icons/${this.#type}.svg"
                      class="mm-notify__icon"
-                     alt="${this._type}">
+                     alt="${this.#type}">
+                     
+                <div class="mm-notify__text-wrapper">
+                   <p class="mm-notify__title">
+                       ${this.#notyTitle}
+                   </p>
+                   
+                   <p class="mm-notify__description">
+                       ${this.#notifyText}
+                   </p>
+                </div>
                 
-                <p class="mm-notify__text">
-                    ${this._notifyText}
-                </p>`;
+                ${this.#hasButton && this.#buttonConfig ? 
+                    `<button onclick="${this.#buttonConfig.callback}" class="mm-notify__callback-btn">
+                        ${this.#buttonConfig.text}
+                    </button>` 
+                : ""}
+                
+                <button class="mm-notify__close">
+                    <img src="./icons/close.svg" alt="Крестик">
+                </button>
+                `;
+
+            templateElem.querySelector(".mm-notify__close").addEventListener('click', () => {
+                this.close();
+            });
+
+            // templateElem.dataset.notifyCounter = `${this.counterId}`;
         }
 
-        this._notyElem = templateElem;
+        this.notyElem = templateElem;
     }
 
     show() {
         let selector = null;
 
-        if (typeof this._selector === "string") {
+        if (typeof this.#selector === "string") {
             // render to .fixed elem
-            const positionArray = this._position.split(" ");
+            const positionArray = this.#position.split(" ");
             if (positionArray.length !== 2) {
                 console.warn("Передан неправильный position в options");
                 return;
@@ -103,56 +114,52 @@ class Notify {
 
             const additionalSelector = positionArray.join(".");
 
-            if (additionalSelector !== "top.right") {
-                selector = document.querySelector(`${this._selector}.${additionalSelector}`);
-            }
-            else {
-                selector = document.querySelector(this._selector);
-            }
+            selector = document.querySelector(`${this.#selector} .${additionalSelector}`);
 
             if (!selector) {
-                const fixedNotiesWrapper = document.createElement("div");
-                fixedNotiesWrapper.classList.add("mm-notifies-wrapper", "fixed", positionArray[0], positionArray[1]);
-                document.body.insertAdjacentElement("beforeend", fixedNotiesWrapper);
-                selector = fixedNotiesWrapper;
+                const sideNotifiesWrapper = document.createElement("div");
+                sideNotifiesWrapper.classList.add("mm-notifies__column-wrapper", positionArray[0], positionArray[1]);
+                document.querySelector(`${this.#selector}`)
+                    .insertAdjacentElement("beforeend", sideNotifiesWrapper);
+                selector = sideNotifiesWrapper;
             }
         }
         else {
-            selector = this._selector;
+            selector = this.#selector;
         }
 
         this.getTemplate();
 
-        if (this._whereToRender) {
+        if (this.#whereToRender) {
             selector.querySelectorAll(".mm-notify__text-noty").forEach(noty => noty.remove());
-            selector.insertAdjacentElement("afterbegin", this._notyElem);
+            selector.insertAdjacentElement("afterbegin", this.notyElem);
         }
         else {
-            selector.insertAdjacentElement("beforeend", this._notyElem);
+            selector.insertAdjacentElement("afterbegin", this.notyElem);
         }
 
-        if (this._cssPosition === "fixed") {
-            this._closeTimeoutFunction = setTimeout(() => {
+        if (this.#cssPosition === "fixed") {
+            this.#closeTimeoutFunction = setTimeout(() => {
                 this.close();
-            }, this._closeTime);
+            }, this.closeTime);
         }
     }
 
     close() {
-        if (this._cssPosition === "fixed") {
-            if (this._closeTimeoutFunction === null) return;
+        if (this.#cssPosition === "fixed") {
+            if (this.#closeTimeoutFunction === null) return;
 
-            this._notyElem.classList.add("closing");
+            this.notyElem.classList.add("closing");
 
-            this._notyElem.addEventListener("transitionend", () => {
-                this._notyElem.remove();
+            this.notyElem.addEventListener("transitionend", () => {
+                this.notyElem.remove();
             })
 
-            this._closeTimeoutFunction = null;
+            this.#closeTimeoutFunction = null;
         }
 
-        if (this._cssPosition === "block") {
-            this._notyElem.remove();
+        if (this.#cssPosition === "block") {
+            this.notyElem.remove();
         }
     }
 }
